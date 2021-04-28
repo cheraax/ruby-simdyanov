48ca52aef02c429a3fc8de2649b0f438e50193d1
andrey.chernov <andrey@chernov.pw>
2021-04-28 11:22:47 +0300

    add init.rb + description

--- init.rb
+++ init.rb
@@ -1,26 +1,27 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 # пишем скрипт, который будет создавать каталог с главой
 # добавлять README.md и коммитить, параметры: путь и количество файлов
 
 path = ARGV[0]
 count = ARGV[1].to_i
-#create directory
-#выходим если не указан путь или каталог уже есть
+# create directory
+# выходим если не указан путь или каталог уже есть
 exit if path.nil? || Dir.exist?(path)
 # создаем если нет каталога
 Dir.mkdir(path)
 # переходим в каталог
 Dir.chdir(path)
-#create README
-File.open("README.md", "w") {|f| f.write("### #{path}") }
-#create app files
-count.times {
-  |i| File.open("app#{i+1}.rb", "w") {|f| f.write("#!/usr/bin/env ruby") }
-}
-#git
+# create README
+File.open('README.md', 'w') { |f| f.write("### #{path}") }
+# create app files
+count.times do |i|
+  File.open("app#{i + 1}.rb", 'w') { |f| f.write('#!/usr/bin/env ruby') }
+end
+# git
 # возвращаемся на каталог выше
-Dir.chdir("..")
-%x(git add ./#{path} )
-%x(git commit -m "init #{path} files")
-%x(git push)
+Dir.chdir('..')
+`git add ./#{path}`
+`git commit -m "init #{path} files"`
+`git push`
