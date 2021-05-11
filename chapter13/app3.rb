9b95ca96d8876c37085af5417145a401adcdb770
andrey.chernov <andrey@chernov.pw>
2021-05-11 17:58:21 +0300

    add app3.rb

--- chapter13/app3.rb
+++ chapter13/app3.rb
@@ -1,1 +1,10 @@
 #!/usr/bin/env ruby
+class Week
+  def each(&block)
+    days = %w[пн вт ср чт пт сб вс]
+    days.each {|d| block.call d}
+  end
+end
+
+week = Week.new
+week.each {|d| print "#{d} "}
