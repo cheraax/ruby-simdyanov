84b340bfa88fea790d304710ca8c3f9d1221fb6b
andrey.chernov <andrey@chernov.pw>
2021-05-11 17:57:20 +0300

    add app1.rb

--- chapter13/app1.rb
+++ chapter13/app1.rb
@@ -1,1 +1,23 @@
 #!/usr/bin/env ruby
+
+class Building
+  attr_accessor :floors, :color, :material, :has_parking, :year, :address
+  def to_s
+    "Building: #{@address}
+    floors: #{@floors}
+    color: #{@color}
+    material: #{@material}
+    parking: #{@has_parking}
+    buid in: #{@year}"
+  end
+end
+
+bld = Building.new
+bld.floors = 5
+bld.color = 'grey'
+bld.material = 'ferroconcrete'
+bld.has_parking = false
+bld.year = 1965
+bld.address = 'Moscow, Mira st. #1'
+
+puts bld
