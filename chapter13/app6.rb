c057fd176f9fbaf882bfd98af7dacb9ee70ea178
andrey.chernov <andrey@chernov.pw>
2021-05-11 18:18:27 +0300

    add app6.rb

--- chapter13/app6.rb
+++ chapter13/app6.rb
@@ -1,1 +1,17 @@
 #!/usr/bin/env ruby
+
+class Integer
+  def minutes
+    self * 60
+  end
+  def hours
+    self * 60.minutes
+  end
+  def days
+    self * 24.hours
+  end
+end
+
+p 5.minutes
+p 1.days
+p 2.hours
