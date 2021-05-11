1b6e442c9885144bb0bb6f184a980af26bd6bb82
andrey.chernov <andrey@chernov.pw>
2021-05-11 17:57:39 +0300

    add app2.rb

--- chapter13/app2.rb
+++ chapter13/app2.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/env ruby
+class ChessFigure
+  attr_accessor :name, :color, :position, :state
+end
+
+pawn = ChessFigure.new
+pawn.name = :pawn
+pawn.color = :white
+pawn.position = :e2
+pawn.state = :alive
+
+p pawn
