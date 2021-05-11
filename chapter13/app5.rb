636d54f29f76bfba71b2b23aab31036a9f7aa71b
andrey.chernov <andrey@chernov.pw>
2021-05-11 17:59:20 +0300

    add app5.rb

--- chapter13/app5.rb
+++ chapter13/app5.rb
@@ -1,1 +1,17 @@
 #!/usr/bin/env ruby
+class Time
+  def self.hello
+    case self.now.hour
+    when 0..4
+      'Good night'
+    when 5..11
+      'Good morning'
+    when 12..17
+      'Good day'
+    when 18..23
+      'Good evening'
+    end
+  end
+end
+
+p Time.hello
