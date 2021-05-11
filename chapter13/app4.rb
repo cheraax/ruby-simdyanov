bb77237e1c3b0e446856c6af04c07d317f60a7ac
andrey.chernov <andrey@chernov.pw>
2021-05-11 17:58:56 +0300

    add app4.rb

--- chapter13/app4.rb
+++ chapter13/app4.rb
@@ -1,1 +1,44 @@
 #!/usr/bin/env ruby
+class Factory
+  @@count = 0
+  @@teaddy_bears = []
+  @@cubes = []
+  @@balls = []
+
+  def build(type = :teaddy_bear)
+    @@count += 1
+    case type
+   when :teaddy_bear
+     @@teaddy_bears << TeaddyBear.new
+     @@teaddy_bears.last
+   when :cube
+     @@cubes << Cube.new
+     @@cubes.last
+   when :ball
+     @@balls << Ball.new
+     @@balls.last
+   end
+  end
+  def total
+    @@count
+  end
+  def offers
+    {teaddy_bear: @@teaddy_bears.size, cube: @@cubes.size, ball: @@balls.size}
+  end
+
+  class TeaddyBear
+  end
+  class Cube
+  end
+  class Ball
+  end
+
+end
+
+types = %i[teaddy_bear cube ball]
+factory = Factory.new
+toys = Array.new(20) { factory.build types.sample }
+
+p factory.total
+p factory.offers
+
