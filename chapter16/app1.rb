5dfd182eca74de2ae3fa4921b8d11ce3cabdefb5
andrey.chernov <andrey@chernov.pw>
2021-05-21 16:22:38 +0300

    ch16 upload

--- chapter16/app1.rb
+++ chapter16/app1.rb
@@ -1,1 +1,26 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
+class Integer
+  BYTES = 1024
+  def kilobytes
+    self * BYTES
+  end
+
+  def megobytes
+    self * BYTES.kilobytes
+  end
+
+  def gigabytes
+    self * BYTES.megobytes
+  end
+
+  def terabytes
+    self * BYTES.gigabytes
+  end
+end
+
+p 1.megobytes
+p 1.kilobytes
+p 1.gigabytes
+p 1.terabytes
--- chapter16/app2.rb
+++ chapter16/app2.rb
@@ -1,1 +1,13 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
+class User
+  attr_accessor :first_name, :last_name, :email
+
+  def initialize
+    yield self if block_given?
+  end
+end
+
+user = User.new { |u| u.email = 'i.petrov@mail.me'; u.first_name = 'ivan'; u.last_name = 'petrov' }
+pp user
