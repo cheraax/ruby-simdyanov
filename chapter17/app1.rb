112a83b5a5be56f397b9c9d035aa8792b831bb44
andrey.chernov <andrey@chernov.pw>
2021-06-09 17:19:01 +0300

    ch17 solutions

--- chapter17/app1.rb
+++ chapter17/app1.rb
@@ -1,1 +1,48 @@
 #!/usr/bin/env ruby
+class Vessel
+  def initialize(*equipment)
+    @equipment = equipment || []
+  end
+  def to_s
+    "vessel with: #{@equipment.join(',')}"
+  end
+end
+
+class Undewater < Vessel
+  def initialize(*equipment)
+    @type = :undewater
+    super
+  end
+  def to_s
+    "#{@type} #{super}"
+  end
+end
+
+class Overwater < Vessel
+  def initialize(*equipment)
+    @type = :overwater
+    super
+  end
+  def to_s
+    "#{@type} #{super}"
+  end
+end
+
+# Атомную подводную лодку (ракеты, торпеды)
+atomic_submarine = Undewater.new :rockets, :torpedos
+puts atomic_submarine.to_s
+# Сухогруз для зерна (трюм, кран)
+cargo_ship = Overwater.new :crane, :cargo
+puts cargo_ship.to_s
+# Контейнеровоз (кран)
+cargo = Overwater.new :crane
+puts cargo.to_s
+# Нефтяной танкер (трюм)
+tanker = Overwater.new :cargo
+puts tanker.to_s
+# Ракетный крейсер (ракеты)
+cruiser = Overwater.new :rockets
+puts cruiser.to_s
+# Военный корабль (ракеты, трюм, кран)
+battleship = Overwater.new :rockets, :crane, :cargo
+puts battleship.to_s
--- chapter17/app2.rb
+++ chapter17/app2.rb
@@ -1,1 +1,59 @@
 #!/usr/bin/env ruby
+class User
+  attr_accessor :name, :email
+  def initialize(name:, email:)
+    @name = name
+    @email = email
+  end
+  def to_s
+    "name: #{@name} email: #{@email}"
+  end
+  alias say to_s
+end
+
+class Admin < User
+  def initialize(name:, email:)
+    super
+    @role = :admin
+  end
+  def to_s
+    "#{super} role: #{@role}"
+  end
+  alias say to_s
+end
+
+class Author < User
+  def initialize(name:, email:)
+    super
+    @role = :author
+  end
+  def to_s
+    "#{super} role: #{@role}"
+  end
+  alias say to_s
+end
+
+class Moderator < User
+  def initialize(name:, email:)
+    super
+    @role = :moderator
+  end
+  def to_s
+    "#{super} role: #{@role}"
+  end
+  alias say to_s
+end
+
+class Visitor < User
+  def initialize(name:, email:)
+    super
+    @role = :visitor
+  end
+  def to_s
+    "#{super} role: #{@role}"
+  end
+  alias say to_s
+end
+
+visitor = Visitor.new name: 'ivan', email: 'ivan@mail.com'
+puts visitor.say
