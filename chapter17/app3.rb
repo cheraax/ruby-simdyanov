5edae342a100dc8bfe528f93651b0cafd15e805f
andrey.chernov <andrey@chernov.pw>
2021-08-09 10:29:20 +0300

    add solution 3

--- chapter17/app3.rb
+++ chapter17/app3.rb
@@ -1,1 +1,26 @@
 #!/usr/bin/env ruby
+class Person
+  private_class_method :new
+  def initialize(name, second_name, last_name, email)
+    @name = name
+    @second_name = second_name
+    @last_name = last_name
+    @email = email
+  end
+  def to_s
+    "#{@last_name} #{@first_name} #{second_name} : #{@email}"
+  end
+end
+
+class User < Person
+  public_class_method :new
+end
+class Admin < Person
+  public_class_method :new
+end
+class Moderator < Person
+  public_class_method :new
+end
+
+prsn = Person.new('ivan', 'ivanovich', 'ivanov', 'ivanov_i_i@email.com')
+usr = User.new('ivan','ivanovich','ivanov','ivanov_i_i@email.com')
